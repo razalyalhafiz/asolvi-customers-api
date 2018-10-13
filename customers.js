5eb635851b2f36c0b072a5a824f3bdfa8aec9c55
Razaly Alhafiz <razalyalhafiz@MacBook-Pro.lan>
2018-10-14 05:12:06 +0800

    test

---
+++ customers.js
@@ -0,0 +1,8 @@
+const customers = [
+    { id: 0, name: 'Robert Smith', status: 'Hot', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg' },
+    { id: 1, name: 'Chris Harris', status: 'Cool', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg' },
+    { id: 2, name: 'Janet Garcia', status: 'Hot', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg' },
+    { id: 3, name: 'Mindy Lee', status: 'Cool', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg' }
+];
+
+module.exports = customers;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const jwksRsa = require('jwks-rsa');
 
 const app = express();
 
+const customers = require("./customers.js")
 const config = require('./config.json');
 const API_AUDIENCE = config.API_AUDIENCE;
 const AUTH0_DOMAIN = config.AUTH0_DOMAIN;
@@ -31,14 +32,6 @@ app.get('/', checkJwt, (req, res) => {
 
 app.get('/customers', checkJwt, (req, res) => {
   console.log(req.user);
-
-  var customers = [
-    { id: 0, name: 'Robert Smith', status: 'Hot', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg' },
-    { id: 1, name: 'Chris Harris', status: 'Cool', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg' },
-    { id: 2, name: 'Janet Garcia', status: 'Hot', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg' },
-    { id: 3, name: 'Mindy Lee', status: 'Cool', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg' }
-  ];
-
   res.json(customers);
 });
 
