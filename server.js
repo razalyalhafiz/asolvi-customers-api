9258e28593c45e7a112d58a756845df1e1a30fc7
Razaly Alhafiz <razalyalhafiz@MacBook-Pro.lan>
2018-10-13 11:27:01 +0800

    mod

--- server.js
+++ server.js
@@ -26,13 +26,12 @@ app.use(cors());
 
 app.get('/', checkJwt, (req, res) => {
   console.log(req.user);
-  //res.send({ hello: 'world' });
   
   var customers = [
-    { id: 0, name: 'Robert Smith', status: 'hot', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg' },
-    { id: 1, name: 'Chris Harris', status: 'cool', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg' },
-    { id: 2, name: 'Janet Garcia', status: 'hot', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg' },
-    { id: 3, name: 'Mindy Lee', status: 'cool', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg' }
+    { id: 0, name: 'Robert Smith', status: 'Hot', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg' },
+    { id: 1, name: 'Chris Harris', status: 'Cool', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg' },
+    { id: 2, name: 'Janet Garcia', status: 'Hot', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg' },
+    { id: 3, name: 'Mindy Lee', status: 'Cool', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg' }
   ];
   
   res.send(customers);
