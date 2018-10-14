2a7f04e37ad14dbcfd1c369a6fcfd517e09f0182
Razaly Alhafiz <razalyalhafiz@MacBook-Pro.lan>
2018-10-15 01:29:28 +0800

    test

--- customers.js
+++ customers.js
@@ -1,8 +1,33 @@
 const customers = [
-    { id: 0, name: 'Robert Smith', status: 'Hot', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg' },
-    { id: 1, name: 'Chris Harris', status: 'Cool', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg' },
-    { id: 2, name: 'Janet Garcia', status: 'Hot', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg' },
-    { id: 3, name: 'Mindy Lee', status: 'Cool', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg' }
-];
+  {
+    id: 0,
+    name: "Robert Smith",
+    company: "AliBaba",
+    status: "Hot",
+    avatar:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg"
+  },
+  {
+    id: 1,
+    name: "Chris Harris",
+    company: "Pharma Niaga",
+    status: "Cool",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg"
+  },
+  {
+    id: 2,
+    name: "Janet Garcia",
+    company: "Nestle",
+    status: "Hot",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg"
+  },
+  {
+    id: 3,
+    name: "Mindy Lee",
+    company: "Google",
+    status: "Cool",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg"
+  }
+]
 
 module.exports = customers;
