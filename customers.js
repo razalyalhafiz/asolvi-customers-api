e0bb0481f6f4fe8bc926c861e207f88b346e3113
Razaly Alhafiz <razalyalhafiz@MacBook-Pro.lan>
2018-10-15 01:36:27 +0800

    test

--- customers.js
+++ customers.js
@@ -2,14 +2,15 @@ const customers = [
   {
     id: 0,
     name: "Robert Smith",
+    position: "Sales Support",
     company: "AliBaba",
     status: "Hot",
-    avatar:
-      "https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg"
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg"
   },
   {
     id: 1,
     name: "Chris Harris",
+    position: "Chief Executive Officer (CEO)",  
     company: "Pharma Niaga",
     status: "Cool",
     avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg"
@@ -17,6 +18,7 @@ const customers = [
   {
     id: 2,
     name: "Janet Garcia",
+    position: "Sales Assistant",
     company: "Nestle",
     status: "Hot",
     avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg"
@@ -24,6 +26,7 @@ const customers = [
   {
     id: 3,
     name: "Mindy Lee",
+    position: "Software Developer",
     company: "Google",
     status: "Cool",
     avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg"
